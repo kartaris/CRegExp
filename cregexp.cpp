9362c06b890763c8607e6d3bacbf368e15b740b9
kartaris <kartaris@users.noreply.github.com>
2021-08-02 06:55:57 +0300

    Indentation

--- cregexp.cpp
+++ cregexp.cpp
@@ -7,14 +7,14 @@ bool matchStar(const char *pattern, const char *subject);
 
 bool matchStar(const char *pattern, const char *subject)
 {
-    for(;;)
-    {
-        if(matchImpl(pattern, subject))
-            return true;
-        if(*subject == '\0')
-            return false;
-        *subject++;
-    }
+   for(;;)
+   {
+      if(matchImpl(pattern, subject))
+         return true;
+      if(*subject == '\0')
+         return false;
+      *subject++;
+   }
 }
 
 /**
@@ -25,46 +25,46 @@ bool matchStar(const char *pattern, const char *subject)
  */
 bool matchImpl(const char *pattern, const char *subject)
 {
-    // End of pattern
-    if(pattern[0] == '\0')
-        return true;
+   // End of pattern
+   if(pattern[0] == '\0')
+      return true;
 
-    // Zero or more any symbol
-    if(pattern[0] == '*')
-        return matchStar(pattern + 1, subject);
+   // Zero or more any symbol
+   if(pattern[0] == '*')
+      return matchStar(pattern + 1, subject);
 
-    // One any symbol
-    if(pattern[0] == '?')
-    {
-        if(subject[0] == '\0')
-            return false;
-        if(pattern[1] == '\0' && subject[1] != '\0')
-            return false;
+   // One any symbol
+   if(pattern[0] == '?')
+   {
+      if(subject[0] == '\0')
+         return false;
+      if(pattern[1] == '\0' && subject[1] != '\0')
+         return false;
 
-        return matchImpl(pattern + 1, subject + 1);
-    }
+      return matchImpl(pattern + 1, subject + 1);
+   }
 
-    if(*subject != '\0' && pattern[0] == *subject)
-    {
-        if(pattern[1] == '\0')
-        {
-            // Pattern doesn't end with ? or * token and there is more symbols in subject
-            return subject[1] == '\0';
-        }
-        return matchImpl(pattern + 1, subject + 1);
-    }
-    return false;
+   if(*subject != '\0' && pattern[0] == *subject)
+   {
+      if(pattern[1] == '\0')
+      {
+         // Pattern doesn't end with ? or * token and there is more symbols in subject
+         return subject[1] == '\0';
+      }
+      return matchImpl(pattern + 1, subject + 1);
+   }
+   return false;
 }
 } // namespace
 
 bool cregexp::match(const char *pattern, const char *subject)
 {
-    // Checking if no pattern given for quit without making any other checks
-    if(!pattern || pattern[0] == '\0')
-        return true;
+   // Checking if no pattern given for quit without making any other checks
+   if(!pattern || pattern[0] == '\0')
+      return true;
 
-    if(pattern[0] != '*' && pattern[0] != '?' && pattern[0] != subject[0])
-        return false;
+   if(pattern[0] != '*' && pattern[0] != '?' && pattern[0] != subject[0])
+      return false;
 
-    return matchImpl(pattern, subject);
+   return matchImpl(pattern, subject);
 }
